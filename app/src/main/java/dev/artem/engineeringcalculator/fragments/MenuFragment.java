863b4e63d8d7a15fe2ab596dc438e90ff4526594
PavelStalone <pavel.shoplik@yandex.ru>
2022-10-02 18:52:37 +0300

    Добавил новый калькулятор CNC.

--- app/src/main/java/dev/artem/engineeringcalculator/fragments/MenuFragment.java
+++ app/src/main/java/dev/artem/engineeringcalculator/fragments/MenuFragment.java
@@ -169,6 +169,10 @@ public class MenuFragment extends Fragment {
                             fragmentTransaction.replace(R.id.big_container, new SwabSurgeFragment(Constant.CALCULATORS_NAMES[position]));
                             break;
 
+                        case 23:
+                            fragmentTransaction.replace(R.id.big_container, new CNCFragment(Constant.CALCULATORS_NAMES[position]));
+                            break;
+
                         default:
                             fragmentTransaction.replace(R.id.big_container, new Calculator1Fragment(Constant.CALCULATORS_NAMES[0]));
                     }
