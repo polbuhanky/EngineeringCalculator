578c2435f58feec813f44ed23d3bda39b288f8d0
polbuhanky <78269098+polbuhanky@users.noreply.github.com>
2022-10-31 20:20:23 +0300

    Сделал структуризацию калькуляторов по папке assets, добавил пару файлов, сделал их сортировку, добавил картинки.

--- app/src/main/java/dev/artem/engineeringcalculator/fragments/EducationFoldersFragment.java
+++ app/src/main/java/dev/artem/engineeringcalculator/fragments/EducationFoldersFragment.java
@@ -1,4 +1,4 @@
-package dev.artem.engineeringcalculator.fragments.PDFSections;
+package dev.artem.engineeringcalculator.fragments;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -20,17 +20,26 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import dev.artem.engineeringcalculator.MainActivity;
 import dev.artem.engineeringcalculator.R;
+import dev.artem.engineeringcalculator.utils.ComparatorFolders;
 
 public class EducationFoldersFragment extends Fragment {
     private RecyclerView menuRV;
-
     MainActivity activity;
     private View rootView;
+    private String path = "";
 
+    public EducationFoldersFragment(String path){
+        if (path != null){
+        this.path = path + "/";}
+    }
     public EducationFoldersFragment(){
+
     }
 
     @Override
@@ -44,7 +53,8 @@ public class EducationFoldersFragment extends Fragment {
         menuRV = rootView.findViewById(R.id.menuRV);
         String[] folders = new String[0];
         try {
-            folders = getContext().getAssets().list("pdfs");
+            folders = getContext().getAssets().list("pdfs" + "/" + path);
+            folders = sortDirectories(folders);
         } catch (IOException e) {
             Toast.makeText(getContext(), "Произошла ошибка при загрузке данных", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
@@ -73,14 +83,19 @@ public class EducationFoldersFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
-            holder.itemTV.setText(data[position]);
+            holder.itemTV.setText(data[position].replace(".pdf", ""));
             holder.backgroundItem.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Log.d("Adapter", "position: " + position);
                     FragmentManager myFragmentManager = ((FragmentActivity)context).getSupportFragmentManager();
                     FragmentTransaction fragmentTransaction = myFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.big_container, new EducationFilesFragment(holder.itemTV.getText().toString()));
+                    String selectedText = data[position];
+                    if (selectedText.contains(".pdf")){
+                        fragmentTransaction.replace(R.id.big_container, new PDFViewerFragment("pdfs" + "/"+ path + selectedText));
+                    }
+                    else {
+                    fragmentTransaction.replace(R.id.big_container, new EducationFoldersFragment(path + selectedText));}
                     fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.commit();
                 }
@@ -104,6 +119,48 @@ public class EducationFoldersFragment extends Fragment {
             }
         }
 
+    }
+    public static String [] sortDirectories(String [] a) {
+        ArrayList<String> arrayForFolders = new ArrayList<>();
+        ArrayList<String> arrayForCalculators = new ArrayList<>();
+        ArrayList<String> arrayForPdfs = new ArrayList<>();
+        String[] resultArray = new String[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].contains("$")) {
+                arrayForCalculators.add(a[i]);
+            }
+            else if(a[i].contains(".pdf")) {
+                arrayForPdfs.add(a[i]);
+            }
+            else {
+                arrayForFolders.add(a[i]);
+            }
+        }
+        for (int i = 0; i < arrayForCalculators.size(); i++) {
+            arrayForCalculators.set(i, arrayForCalculators.get(i).replaceFirst(" ", "~"));
+        }
+        for (int i = 0; i < arrayForFolders.size(); i++) {
+            arrayForFolders.set(i, arrayForFolders.get(i).replaceFirst(" ", "~"));
+        }
+        for (int i = 0; i < arrayForPdfs.size(); i++) {
+            arrayForPdfs.set(i, arrayForPdfs.get(i).replaceFirst(" ", "~"));
+        }
+        Collections.sort(arrayForFolders, new ComparatorFolders());
+        Collections.sort(arrayForCalculators, new ComparatorFolders());
+        Collections.sort(arrayForPdfs, new ComparatorFolders());
+        for (int i = 0; i < arrayForFolders.size(); i++) {
+            resultArray[i] = arrayForFolders.get(i);
+        }
+        for (int i = 0; i < arrayForPdfs.size(); i++) {
+            resultArray[arrayForFolders.size() + i] = arrayForPdfs.get(i);
+        }
+        for (int i = 0; i < arrayForCalculators.size(); i++) {
+            resultArray[arrayForFolders.size() + arrayForPdfs.size() + i] = arrayForCalculators.get(i);
+        }
+        for (int i = 0; i < resultArray.length; i++) {
+            resultArray[i] = resultArray[i].replaceFirst("~", " ");
+        }
+        return resultArray;
     }
 }
 
